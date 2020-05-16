c9b1150956e05c1af5a7ce587d852fae213fb6c1
Dorian <dorian.thivolle@gmail.com>
2020-05-16 13:28:00 +0200

    remove useless things in gerer-*.php

--- views/gerer-representations.php
+++ views/gerer-representations.php
@@ -1,16 +1,3 @@
 <?php if(!defined('_DTLR')) exit('Unauthorized'); ?>
 
 <h1>Gérer les représentations</h1>
-
-<?php
-
-if(isset($_GET['numS']) && isset($_GET['action'])) {
-	echo 'ok';
-}
-
-else {
-	$req = "SELECT * FROM LesTickets";
-	$cursor = $this->db->execute($req);
-
-	// ...
-}
--- views/gerer-reservations.php
+++ views/gerer-reservations.php
@@ -1,28 +1,3 @@
 <?php if(!defined('_DTLR')) exit('Unauthorized'); ?>
 
 <h1>Gérer les réservations</h1>
-
-<?php
-
-if(isset($_GET['noSerie']) && isset($_GET['action']) && $_GET['action'] == 'supprimer') {
-	echo 'ok';
-}
-
-else if(isset($_GET['noSerie']) && isset($_GET['action']) && $_GET['action'] == 'modifier') {
-	echo 'ok';
-}
-
-else if(isset($_GET['action']) && $_GET['action'] == 'ajouter') {
-	echo 'ajout';
-}
-
-else {
-	$req = "SELECT * FROM LesTickets";
-	$cursor = $this->db->execute($req);
-	
-	$row = oci_fetch_array($cursor, OCI_ASSOC);
-
-	echo '<a href="?modifier&noSerie=256">Modifier</a>';
-
-	// ...
-}
